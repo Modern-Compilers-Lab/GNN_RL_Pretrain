0a1111a3d4dabe7aabeb48971346df1697221f92
Musa Khan <mk8737@nyu.edu>
2025-06-26 15:59:07 +0400

    finalized testing_job.sh; reusable script for future slurm jobs

--- testing_job.sh
+++ testing_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -c 1
 #SBATCH -t 01:00:00
-#SBATCH -o /scratch/mk8737/GNN_RL_Pretrain/tmp_slurm_%j.out
-#SBATCH -e /scratch/mk8737/GNN_RL_Pretrain/tmp_slurm_%j.err
+#SBATCH -o /scratch/mk8737/rl_pretrain/tmp_slurm_%j.out
+#SBATCH -e /scratch/mk8737/rl_pretrain/tmp_slurm_%j.err
 
 module purge
 module load cuda/11.8.0
@@ -12,8 +12,7 @@ source /share/apps/NYUAD5/miniconda/3-4.11.0/bin/activate
 conda activate rl_pretrain
 
 # Copy outputs into designated directory
-SCRATCH_BASE=/scratch/mk8737/GNN_RL_Pretrain
-RESULTS_DIR=$SCRATCH_BASE/results/testing_slurm_$SLURM_JOB_ID
+RESULTS_DIR=$RL_PRETRAIN_SCRATCH/results/testing_slurm_$SLURM_JOB_ID
 mkdir -p $RESULTS_DIR
 
 # Copy code into scratch
@@ -23,8 +22,8 @@ mkdir -p $RESULTS_DIR
 # Run training
 python testing_job.py
 
-mv $SCRATCH_BASE/tmp_slurm_${SLURM_JOB_ID}.out $RESULTS_DIR/job_${SLURM_JOB_ID}.out
-mv $SCRATCH_BASE/tmp_slurm_${SLURM_JOB_ID}.err $RESULTS_DIR/job_${SLURM_JOB_ID}.err
+mv $RL_PRETRAIN_SCRATCH/tmp_slurm_${SLURM_JOB_ID}.out $RESULTS_DIR/job_${SLURM_JOB_ID}.out
+mv $RL_PRETRAIN_SCRATCH/tmp_slurm_${SLURM_JOB_ID}.err $RESULTS_DIR/job_${SLURM_JOB_ID}.err
 
 # Save results (logs, model checkpoints, etc.)
 #mkdir -p ~/capstone/GNN_RL_Pretrain/results/testing_slurm_$SLURM_JOB_ID
